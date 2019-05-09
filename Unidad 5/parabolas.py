4a2efaa058ac50e19da26db9e8d13a2bfaf96947
Hector Rodriguez <=>
2019-05-09 10:59:45 -0500

    cambios

--- Unidad 5/parabolas.py
+++ Unidad 5/parabolas.py
@@ -1,20 +1,20 @@
 import math
 
-a = 500
-b = 1800
-n = 2
+a = 2
+b = 7
+n = 22822
 h = (b-a) / n
 
 def f(x):
-    return math.sin(x)
+    return (1/x)
 
-# x = [ a + (i*h) for i in range(n+1) ]
-x = [500, 1150, 1800]
-print('x = {}'.format(x))
+x = [ a + (i*h) for i in range(n+1) ]
+# x = [500, 1150, 1800]
+# print('x = {}'.format(x))
 
-# fx = [ f(xi) for xi in x ]
-fx = [ 9, 16.1, 23 ]
-print('fx = {}'.format(fx))
+fx = [ f(xi) for xi in x ]
+# fx = [ 9, 16.1, 23 ]
+# print('fx = {}'.format(fx))
 
 S = (h/3) * (fx[0] + ( 2 * (sum([ fx[xi] for xi in range(2, len(fx)-2, 2) ])) ) + ( 4 * (sum([ fx[xi] for xi in range(1, len(fx)-1, 2) ])) ) + fx[len(fx)-1])
 print('S = {}'.format(S))
--- Unidad 5/trapecios.py
+++ Unidad 5/trapecios.py
@@ -1,20 +1,20 @@
 import math
 
-a = 500
-b = 1800
-n = 1
+a = 2
+b = 7
+n = 22822
 h = (b-a) / n
 
 def f(x):
-    return math.sin(x)
+    return (1 / x)
 
-# x = [ a + (i*h) for i in range(n+1) ]
-x = [500, 1800]
-print('x = {}'.format(x))
+x = [ a + (i*h) for i in range(n+1) ]
+# x = [500, 1800]
+# print('x = {}'.format(x))
 
-# fx = [ f(xi) for xi in x ]
-fx = [ 9, 23 ]
-print('fx = {}'.format(fx))
+fx = [ f(xi) for xi in x ]
+# fx = [ 9, 23 ]
+# print('fx = {}'.format(fx))
 
 T = (h/2) * (fx[0] + ( 2 * (sum([ fx[xi] for xi in range(1, len(fx)-1) ])) ) + fx[len(fx)-1])
 print('T = {}'.format(T))
